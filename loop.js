f5b33548c81eee9e138e4f33059acd777d14fd96
shamim reza <opt.shamim@gmail.com>
2025-12-02 07:44:31 +0600

    loop first commit

---
+++ loop.js
@@ -0,0 +1,18 @@
+for (let i = 0; i <= 5; i++) {
+    console.log("Printing 1 to 5, Now Showing", i)
+}
+
+
+for (let i =0; i <= 40; i++ ){
+    if (i%2 === 0 && i > 0) {
+        console.log("Even Number betweem 1 to 40", i);
+    }
+}
+
+
+let sum = 0;
+for (let i =0; i <= 100; i++ ){
+    sum +=i;
+}
+
+console.log("sum is ", sum)
